0ab996ca54dcda3e6de64e19c71dd963d4cec68a
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2023-05-05 15:25:30 +0300

    pep8

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
 
-
 app = Flask(__name__)
 
 """Создаем список чтобы не использовать базы данных"""
 ads = []
 
-
 """Функция создания объявлений"""
+
+
 @app.route('/ads', methods=['POST'])
 def create_ad():
     data = request.get_json()
     current_time = datetime.now()
-    new_ad = {'title': data['title'], 'description': data['description'], 'creation_date': current_time, 'owner': data['owner']}
+    new_ad = {'title': data['title'], 'description': data['description'], 'creation_date': current_time,
+              'owner': data['owner']}
     ads.append(new_ad)
     return jsonify({'ad': new_ad})
 
 
 """Функция получения объявлений"""
+
+
 @app.route('/ads/<int:ad_id>', methods=['GET'])
 def get_ad(ad_id):
     for ad in ads:
@@ -28,6 +31,8 @@ def get_ad(ad_id):
 
 
 """Функция получения объявлений"""
+
+
 @app.route('/ads/<int:ad_id>', methods=['DELETE'])
 def delete_ad(ad_id):
     for ad in ads:
